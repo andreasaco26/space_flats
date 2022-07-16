0feac6c7f724236d000cb4afd8d040275001bf7c
cesar banchio <cbanchio5@gmail.com>
2022-07-16 18:11:13 +0200

    some photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Flat.create(address: "Pluto Hill n.9, Pluto", description: "Great for people searching for a quiet place.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliqu")
-Flat.create(address: "Sun aveunue 1, Sun", description: "If you like a warm place .Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliqu")
+require 'open-uri'
+flat1 = Flat.create(address: "Pluto Hill n.9, Pluto", description: "Great for people searching for a quiet place.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliqu")
+# flat2 = Flat.create(address: "Sun aveunue 1, Sun", description: "If you like a warm place .Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliqu")
+
+file = URI.open('https://loveincorporated.blob.core.windows.net/contentimages/gallery/7402eabd-e39f-4ea6-923f-24a975b54459-ice-house.jpg')
+flat1.photos.attach(io: file, filename: '7402eabd-e39f-4ea6-923f-24a975b54459-ice-house.jpg', content_type: 'image/jpg')
+flat1.save
