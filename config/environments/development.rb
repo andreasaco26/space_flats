a5a8cd1323f9e79b6eb67b8ef0ffe9ed5d46c1f0
cesar banchio <cbanchio5@gmail.com>
2022-07-16 17:09:22 +0200

    cloudinary1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Active storage & Cloudinary
+  config.active_storage.service = :cloudinary
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
