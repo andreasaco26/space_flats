9775bdc874ed07691e79687842ca9e47cd6306c9
andrea saco <andrea.saco26@gmail.com>
2022-07-21 21:51:34 +0200

    added my key, added one font, change home.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_132622) do
+ActiveRecord::Schema.define(version: 2022_07_19_193851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,11 @@ ActiveRecord::Schema.define(version: 2022_07_16_132622) do
   end
 
   create_table "flats", force: :cascade do |t|
-    t.string "price"
+    t.string "address"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
