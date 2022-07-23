8c06866672c7f27b51bb9438040416595336d421
andrea saco <andrea.saco26@gmail.com>
2022-07-23 11:42:20 +0200

    done landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   layout "home.html.erb", only: :home
   def home
+    @flats = Flat.all.order(created_at: :desc)
   end
 end
